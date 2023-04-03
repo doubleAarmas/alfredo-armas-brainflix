12a4373bb6406802efe50f1a56ba4dd019618fa5
Alfredo Armas <aarma028@gmail.com>
2023-04-03 15:43:32 -0400

    initial prep completed

---
+++ src/components/Component/Header.js
@@ -0,0 +1,11 @@
+import logo from "./Brainflix-logo.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default Header;
